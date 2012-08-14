9e773e9dbdbe2c64665a1de81b1c9c800cf34b06
sevakd <sevak.gd@gmail.com>
2012-08-14 15:04:43 -0400

    created connection to sql server

--- services/services.php
+++ services/services.php
@@ -2,6 +2,11 @@
 require_once('PhpConsole.php');
 PhpConsole::start();
 
+$con = mysql_connect('localhost','cartographer','sourcemap');
+if (!$con){
+    die('could not connect: ' . mysql_error());
+}
+
 $uppAr = json_decode($_POST['suppObj'], true); //returns json obj as assoc arr
 
 foreach ($uppAr->supplychain->stops as $node){ //iter through stops
